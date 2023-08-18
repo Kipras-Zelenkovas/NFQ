5c3d2204607a4d0f4c766792cfdf31248f65387c
kazkas <kipraszelenkovas@gmail.com>
2023-08-19 01:21:01 +0300

    Authentication for specialist and aproximate app routes.

---
+++ backend/app/Http/Controllers/Specialist/Auth.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Specialist;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
+use Illuminate\Support\Facades\Hash;
+
+class Auth extends Controller
+{
+
+    public function login(Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'email'     => 'required|email|max:40',
+                'password'  => 'required|min:8|max:30'
+            ]);
+
+            $user = User::where('email', $validated['email'])->first();
+
+            if (!$user || !Hash::check($validated['password'], $user->password)) {
+                return response()->json([
+                    'status'    => false,
+                    'message'   => 'Invalid credentials'
+                ]);
+            }
+
+            $token = $user->createToken("API_TOKEN" . rand(1569, 945214))->plainTextToken;
+
+            return response()->json([
+                'status'    => true,
+                'token' => $token,
+                'message'   => 'Successful login'
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage());
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            $user = User::where('email', $request->email)->first();
+
+            $user->tokens()->delete();
+
+            return response()->json([
+                'status'    => true,
+                'message'   => 'Successful logout'
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), 500);
+        }
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Specialist\Auth;
+use App\Http\Controllers\User\Reservation;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+
+Route::prefix('specialist')->group(function () {
+    Route::post('login', [Auth::class, 'login'])->middleware('guest')->name('login');
+    Route::post('logout', [Auth::class, 'logout'])->middleware('auth:sanctum')->name('logout');
+
+
+    Route::get('test', function (Request $request) {
+        $res = User::find($request->user()->id);
+        return response()->json([
+            'data'  => $res->reservations,
+        ]);
+    })->middleware('auth:sanctum')->name('test');
+});
+
+
+Route::prefix('user')->group(function () {
+    Route::post('reserve', [Reservation::class, 'reserve'])->middleware('guest')->name('user.reserve');
+    Route::post('cancel', [Reservation::class, 'cancel'])->middleware('guest')->name('user.cancel');
+    Route::get('check', [Reservation::class, 'check'])->middleware('guest')->name('user.check');
 });
